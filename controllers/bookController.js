2c111ce63d7c13bb774d4fa0fe71a2f427490553
himethdaha <himethdaha@gmail.com>
2022-09-28 12:52:07 -0400

    connecting create book controller with the view

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,6 +6,7 @@ const Genre = require('../models/genre')
 const async = require('async')
 const bookinstance = require('../models/bookInstance')
 const book = require('../models/book')
+const {body,validationResult} = require('express-validator')
 
 //Will be the sites Index page
 exports.index=(req,res)=>{
@@ -91,13 +92,121 @@ exports.book_detail=(req,res,next)=>{
     )
 }
 //Display book create form
-exports.book_create_get=(req,res)=>{
-    res.send(`book create form GEt`)
+exports.book_create_get=(req,res,next)=>{
+   //Get the author and genres of the book
+   async.parallel({
+    authors(callback)
+    {
+        Author.find(callback)
+    },
+    genres(callback)
+    {
+        Genre.find(callback)
+    }
+   },
+   //If an error occurs while finding the author and genres, pass it to express
+   (err,results)=>{
+    if(err)
+    {
+        return next(err)
+    }
+    {
+        res.render('bookCreate_form',{
+            title:'Create book',
+            authors:results.authors,
+            genres:results.genres
+        })
+    }
+   }
+   )
+
 }
 //Handle book create form
-exports.book_create_post=(req,res)=>{
-    res.send(`book create form POST`)
-}
+exports.book_create_post=[
+    //Convert the genres into an array to be used by the view 
+    (req,res,next)=>{
+        if(!Array.isArray(req.body.genre)){
+            //When posting the form if it's the 1st post convert the genres to an array and if it's an update/delete/re-render keep it in the same type (array)
+            req.body.genre = typeof req.body.genre === 'undefined' ? [] : [req.body.genre]
+        }
+        next()
+    },
+
+    //Validate and sanitize data
+    body('title',"Title is required").trim().isLength({min:1}).escape(),
+    body('author', "Author is required").trim().isLength({min:1}).escape(),
+    //Sanitize each genre in the array
+    body('genre.*').escape(),
+    body('isbn', "ISBN is required").trim().isLength({min:4}).escape(),
+    body('summary', 'Summary is required').trim().isLength({min:10}).escape(),
+
+    (req,res,next)=>{
+        //If there are validation errors, save them to a variable to be later displayed in the view
+        const errors = validationResult(req)
+
+        //Book object with the invalid data or valid data
+        const book = new Book({
+            title:req.body.title,
+            author:req.body.author,
+            genre:req.body.genre,
+            isbn:req.body.isbn,
+            summary:req.body.summary
+        });
+
+        //If errors were found
+        if(!errors.isEmpty())
+        {
+            //Re-render the form with error messages and sanitized user values
+            async.parallel({
+                authors(callback)
+                {
+                    Author.find(callback)
+                },
+                genres(callback)
+                {
+                    Genre.find(callback)
+                }
+            },
+            //If errors occured while finding, pass it to express
+            (err,results)=>{
+                if(err)
+                {
+                    return next(err)
+                }
+
+                //Mark the previously selected genres as checked
+                for(const genre of results.genres)
+                {
+                    if(book.genre.includes(genre._id))
+                    {
+                        genre.checked = 'true'
+                    }
+                }
+
+                //Render the form with the values/errors
+                res.render('bookCreate_form',{
+                    title:'Create Book',
+                    authors:results.authors,
+                    genres:results.genres,
+                    book,
+                    errors:errors.array()
+                })
+            }
+            )
+            return;
+        }
+
+        //If data is valid save the book to the database
+        book.save((err)=>{
+            if(err)
+            {
+                return next(err)
+            }
+            res.redirect(book.url)
+        })
+    }
+
+]
 //Display book delete form
 exports.book_delete_get=(req,res)=>{
     res.send(`book delete form GET`)
