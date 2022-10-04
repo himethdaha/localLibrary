a92d4d3984959044a4f7fd0409b54f74a9dca4e6
himethdaha <himethdaha@gmail.com>
2022-10-04 18:22:43 -0400

    Hide config

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const {body,validationResult} = require('express-validator')
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-
+const bcrypt = require('bcryptjs')
 
 //User Sign up
 exports.signup_get=(req,res,next)=>{
@@ -21,12 +21,15 @@ exports.signup_post=[
     (req,res,next)=>{
         //Save errors in a variable
         const errors = validationResult(req)
+    
 
         //Create a user object
         const user = new User({
             username:req.body.username,
             password:req.body.password
         });
+        
+        
 
         //If errors are present re-render form with errors
         if(!errors.isEmpty())
@@ -45,7 +48,7 @@ exports.signup_post=[
             {
                 return next(err)
             }
-                res.redirect('/login')
+                res.redirect('/')
         })
         
     }
@@ -64,17 +67,20 @@ exports.login_get=(req,res,next)=>{
 //For user Authentication
 passport.use(
     new LocalStrategy((username, password, done) => {
-      User.findOne({ username: username }, (err, user) => {
+      User.findOne({ username: username }, async(err, user) => {
         if (err) { 
           return done(err);
         }
         if (!user) {
-          return done(null, false, { message: "Incorrect username" });
+          return done(null, false, { message: "Incorrect Password or Username" });
         }
-        if (user.password !== password) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
+       if(await user.comparePassword(password, user.password)){
+            return done(null, user);
+       }
+       else
+       {
+        return done(null,false,{message:"Incorrect Password or Username"});
+       }
       });
     })
   );
--- models/user.js
+++ models/user.js
@@ -1,9 +1,29 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
 
 const userSchema = new Schema({
     username:{type:String, minLength:3,required:true},
     password:{type:String, minLength:8,required:true}
 })
+
+userSchema.pre('save',function(next){
+    let user = this;
+
+    bcrypt.hash(user.password,10,function(err,hash){
+        if(err)
+        {
+            return next(err)
+        }
+        user.password = hash
+        next()
+
+    })
+
+})
+userSchema.methods.comparePassword = async function(userPassword,dbPassword)
+{
+   return await bcrypt.compare(userPassword,dbPassword)
+}
 
 module.exports = mongoose.model('User',userSchema)
